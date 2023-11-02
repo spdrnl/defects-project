928d0df80ea357d4800c991e1ca1247b47300365
Sanne de Roever <sanne.de.roever@gmail.com>
2023-11-02 14:38:20 +0100

    Workflows sets for log reg.

--- src/models.R
+++ src/models.R
@@ -1,4 +1,7 @@
 library('ProjectTemplate')
+library('tidyverse')
+library('tidymodels')
+
 load.project()
 
 # Prepare v-fold cross-validation
@@ -6,19 +9,29 @@ load.project()
 train_folds = vfold_cv(train)
 
 # Try logistic model workflow
-log_reg_spec <- logistic_reg(penalty = tune()) %>%
-  set_engine("glmnet")
-
-regression_recipe <- recipe(defects ~ ., data=train) %>%
+basic_rec <- recipe(defects ~ ., data=train) %>%
   step_corr(threshold = 0.90) %>%
   step_best_normalize(all_numeric_predictors()) %>%
   step_interact(terms = ~ all_numeric_predictors():all_numeric_predictors()) %>%
   step_normalize() %>%
   step_zv()
 
-log_reg_workflow <- workflow() %>%
-  add_recipe(regression_recipe) %>%
-  add_model(log_reg_spec)
+interaction_rec <- recipe(defects ~ ., data=train) %>%
+  step_corr(threshold = 0.90) %>%
+  step_best_normalize(all_numeric_predictors()) %>%
+  step_interact(terms = ~ all_numeric_predictors():all_numeric_predictors()) %>%
+  step_normalize() %>%
+  step_zv()
+
+preproc <-
+  list(basic = basic_rec,
+       interact = interaction_rec
+  )
+
+log_reg_spec <- logistic_reg(penalty = tune()) %>%
+  set_engine("glmnet")
+
+log_reg_wfs <- workflow_set(preproc, list(lm = log_reg_spec), cross = FALSE)
 
 # Tune hyper-parameters
 all_cores <- parallel::detectCores(logical = FALSE) / 2
@@ -29,16 +42,20 @@ registerDoParallel(cl)
 
 grid_control <- control_grid(
   allow_par = TRUE,
+  save_pred = TRUE,
+  event_level = "second",
   parallel_over = "resamples"
 )
 
-startTime <- Sys.time() 
-log_reg_tune_res <- log_reg_workflow %>% 
-  tune_grid(
-    resamples = train_folds,
-    control = grid_control
-  )
-endTime <- Sys.time() 
+startTime <- Sys.time()
+log_reg_wfs_tune <- log_reg_wfs %>%
+  workflow_map(fn = "tune_grid",
+               grid = 10,
+               seed = 1101,
+               verbose = TRUE,
+               resamples = train_folds,
+               control = grid_control)
+endTime <- Sys.time()
 print(endTime - startTime)
 
 
@@ -48,7 +65,7 @@ log_reg_final <-
   fit(train)
 
 # Predict the test set
-log_reg_final %>% 
-  predict(test) %>% 
-  bind_cols(test) 
+log_reg_final %>%
+  predict(test) %>%
+  bind_cols(test)
 
