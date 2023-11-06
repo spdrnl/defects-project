42667977a1f44f0cd8925f4436deaa6966160814
Sanne de Roever <sanne.de.roever@gmail.com>
2023-11-06 16:17:02 +0100

    Roc Auc .789.

--- src/models.R
+++ src/models.R
@@ -4,28 +4,41 @@ library('tidymodels')
 
 load.project()
 
-# Prepare v-fold cross-validation
+#[1] "loc"               "v.g."              "ev.g."             "iv.g."
+#[5] "n"                 "v"                 "l"                 "d"
+#[9] "i"                 "e"                 "b"                 "t"
+#[13] "lOCode"            "lOComment"         "lOBlank"           "locCodeAndComment"
+#[17] "uniq_Op"           "uniq_Opnd"         "total_Op"          "total_Opnd"
+#[21] "branchCount"       "defects"
 
+# Prepare v-fold cross-validation
+set.seed(123)
 train_folds = vfold_cv(train)
 
 # Try logistic model workflow
 basic_rec <- recipe(defects ~ ., data=train) %>%
-  step_corr(threshold = 0.90) %>%
   step_best_normalize(all_numeric_predictors()) %>%
-  step_interact(terms = ~ all_numeric_predictors():all_numeric_predictors()) %>%
+  step_corr(threshold = 0.90) %>%
   step_normalize() %>%
   step_zv()
 
 interaction_rec <- recipe(defects ~ ., data=train) %>%
-  step_corr(threshold = 0.90) %>%
-  step_best_normalize(all_numeric_predictors()) %>%
   step_interact(terms = ~ all_numeric_predictors():all_numeric_predictors()) %>%
+  step_best_normalize(all_numeric_predictors()) %>%
+  step_corr(threshold = 0.90) %>%
+  step_normalize() %>%
+  step_zv()
+
+ns_rec <- recipe(defects ~ ., data=train) %>%
+  step_ns(all_numeric_predictors(), deg_free = 5) %>%
   step_normalize() %>%
   step_zv()
 
 preproc <-
-  list(basic = basic_rec,
-       interact = interaction_rec
+  list(
+       #basic = basic_rec,
+       #interact = interaction_rec,
+       ns = ns_rec
   )
 
 log_reg_spec <- logistic_reg(penalty = tune()) %>%
@@ -34,7 +47,7 @@ log_reg_spec <- logistic_reg(penalty = tune()) %>%
 log_reg_wfs <- workflow_set(preproc, list(lm = log_reg_spec), cross = FALSE)
 
 # Tune hyper-parameters
-all_cores <- parallel::detectCores(logical = FALSE) / 2
+all_cores <- parallel::detectCores(logical = FALSE) / 4
 
 library(doParallel)
 cl <- makePSOCKcluster(all_cores)
@@ -51,21 +64,14 @@ startTime <- Sys.time()
 log_reg_wfs_tune <- log_reg_wfs %>%
   workflow_map(fn = "tune_grid",
                grid = 10,
-               seed = 1101,
+               seed = 1100,
                verbose = TRUE,
                resamples = train_folds,
                control = grid_control)
 endTime <- Sys.time()
 print(endTime - startTime)
 
+collect_metrics(log_reg_wfs_tune)
 
-# Finalize the model with the best parameters on the complete train set
-log_reg_final <-
-  finalize_workflow(log_reg_workflow, select_best(log_reg_tune_res)) %>%
-  fit(train)
 
-# Predict the test set
-log_reg_final %>%
-  predict(test) %>%
-  bind_cols(test)
 
